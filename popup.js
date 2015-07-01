195395a8242a26f60138a182e359abe0433ff0f8
Arjun Mehta <arjunmehta94@gmail.com>
2015-07-01 15:44:21 +0530

    Initial trials working
    Able to choose tabs, separate them

--- popup.js
+++ popup.js
@@ -1,48 +1,37 @@
-// function list_session(callback){
-// 	chrome.windows.getAll({populate:true}, function(window_list){
-// 		var list = [];
-// 		for(var i = 0; i<window_list.length; i++){
-// 			list = list.concat(window_list[i].tabs);
-// 		}
-// 		//console.log(list);
-// 		if(callback){
-// 			callback(list);
-// 		}
-// 	});
-// }
-
 chrome.windows.getCurrent(function(win)
 {
 	chrome.tabs.getAllInWindow(win.id, function(tabs)
 	{
+		obj = {}; // title, tabId dictionary
 		for (var i = 0; i<tabs.length; i++)
 		{
-			
+			obj[tabs[i].title] = tabs[i].id; 
 			var item = $("<input type='checkbox'/>")
-			.attr("name", tabs[i].title)
+			.attr("name", tabs[i].title) // store form element, input type as a checkbox
 			.attr("value", tabs[i].url)
 			.attr("id", tabs[i].title)
 			.attr("url", tabs[i].url)
 			.val(tabs[i].title);
-			var s = "<label for=" + tabs[i].title + ">" + tabs[i].title + "</label>";
-			item.appendTo("#activeTabs");
-			//console.log(tabs[i].title);	
-			//$("#activeTabs").after(item);
-			item.after(s);	
-			//console.log(item.val());
+			var s = "<label for=" + tabs[i].title + ">" + tabs[i].title + "</label>"; // create label item for text
+			item.appendTo("#activeTabs"); // append form element
+			item.after(s);	// append label
+			
 		}
-	});
-	$("#btn-separate").on('click', function(){
-		var selected = [];
-		$("#activeTabs input:checked").each(function(){
-			selected.push($(this).attr('url'));
+		$("#btn-separate").on('click', function(){ // if separate is clicked
+			var selected = [];
+			var selectedId = [];
+			$("#activeTabs input:checked").each(function(){ // separate tabs which are selected if button clicked
+				selected.push($(this).attr('url')); 
+				selectedId.push(obj[$(this).attr("name")]); 
+			});
+			//console.log(selected[0]);
+			chrome.windows.create({type: 'normal', url: selected}); // put selected tabs in new window
+			chrome.tabs.remove(selectedId); // delete selected tabs from current window
 		});
-		//console.log(selected[0]);
-		chrome.windows.create({type: 'normal', url: selected[0]});
 	});
+	
 
 });
 
 
-//console.log(selected);
 
