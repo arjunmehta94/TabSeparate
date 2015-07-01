daa0f13fbbbe6cf0150c8e8787b36139cb60f559
Arjun Mehta <arjunmehta94@gmail.com>
2015-07-01 11:04:01 +0530

    added button click functionality
    when separate button is clicked, it now creates a new window of only 1
    tab.

--- popup.js
+++ popup.js
@@ -22,13 +22,27 @@ chrome.windows.getCurrent(function(win)
 			.attr("name", tabs[i].title)
 			.attr("value", tabs[i].url)
 			.attr("id", tabs[i].title)
+			.attr("url", tabs[i].url)
 			.val(tabs[i].title);
 			var s = "<label for=" + tabs[i].title + ">" + tabs[i].title + "</label>";
 			item.appendTo("#activeTabs");
 			//console.log(tabs[i].title);	
 			//$("#activeTabs").after(item);
 			item.after(s);	
-			console.log(item.val());
+			//console.log(item.val());
 		}
 	});
+	$("#btn-separate").on('click', function(){
+		var selected = [];
+		$("#activeTabs input:checked").each(function(){
+			selected.push($(this).attr('url'));
+		});
+		//console.log(selected[0]);
+		chrome.windows.create({type: 'normal', url: selected[0]});
+	});
+
 });
+
+
+//console.log(selected);
+
