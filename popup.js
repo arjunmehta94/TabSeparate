9f0463bee36dc97db3369b5e0708b239c1d6f5b1
Arjun Mehta <arjunmehta94@gmail.com>
2015-07-08 10:45:11 +0530

    add move functionality and cookie.js file

--- popup.js
+++ popup.js
@@ -1,24 +1,17 @@
-
+var xval = 0;
+winId = "";
+$.cookie("winId", winId);
 chrome.windows.getCurrent(function(win)
 {
 	chrome.tabs.getAllInWindow(win.id, function(tabs)
 	{
-		//console.log(tabs);
-		obj = {}; // title, tabId dictionary
-		// for (var i = 0; i<tabs.length; i++)
-		// {
-		// 	obj[tabs[i].title + i.toString()] = tabs[i].id; 
-		// 	var item = returnFormElement(tabs[i], i);
-		// 	var s = generateLabelForFormElement(tabs[i]); // create label item for text
-		// 	item.appendTo("#activeTabs"); // append form element
-		// 	//item.after(s);	// append label
-			
-		// }
-		updateUI(tabs, obj);
+		updateUI(tabs, win.id);
 		$("#activeTabs").children().on('click', function(){
 			if(!$(this).prop("checked")){
 				$(this).prop("checked", true);
 				$(this).addClass("bg-success");
+				// highlightinfo = {tabs: [parseInt($(this).attr("id"))]}
+				// chrome.tabs.highlight(highlightinfo);
 			}
 			else{
 				$(this).prop("checked", false);
@@ -26,12 +19,12 @@ chrome.windows.getCurrent(function(win)
 			}
 			
 		});
+		
 		$("#btn-separate").on('click', separate); // if separate is clicked
 		$("#btn-close").on('click', close); // if closed is clicked
 		//updateUI(tabs, obj);
 	});
 });
-// console.log(selectedId);
 
 function returnFormElement(tab, i){
 	var item = $("<div>" + (i+1).toString() + ": " + tab.title + "</div>")
@@ -45,54 +38,59 @@ function returnFormElement(tab, i){
 	return item;
 }
 
-function generateLabelForFormElement(tab){
-	var s = "<label for=" + tab.title + ">" + tab.title + "</label></br>";
-	return s;
-}
-
 function separate(){
 	var selected = [];
 	var selectedId = [];
-	// $("#activeTabs input:checked").each(function(){ // separate tabs which are selected if button clicked
-	// 	selected.push($(this).attr("url")); 
-	// 	selectedId.push(obj[$(this).attr("name") + $(this).attr("num")]); 
-	// });
+	
 	$(".tabs").each(function(){
 		if($(this).prop("checked")){
 			selected.push($(this).attr("url")); 
-			selectedId.push(obj[$(this).attr("name") + $(this).attr("num")]);
+			selectedId.push(parseInt($(this).attr("id")));
 		}
 	});
 	if(selected.length > 0){
-		chrome.windows.create({type: 'normal', url: selected}); // put selected tabs in new window
-		chrome.tabs.remove(selectedId); // delete selected tabs from current window
+		
+		chrome.windows.create({type: 'normal'}, function(win){
+			var winId = $.cookie("winId").toString();
+			winId += win.id.toString() + " ";
+			$.cookie("winId", winId);
+			var winId = $.cookie("winId");
+			var result = winId.split(" ");
+			result.splice(result.length - 1, 1);
+			var x = parseInt(result[result.length - 1]);
+			chrome.tabs.move(selectedId, {"windowId":x, "index":-1});
+			// chrome.tabs.getAllInWindow(x, function(tabs){
+			// 	chrome.tabs.remove(tabs[0].id);
+			// });
+		}); 
 	}
 	//updateUI(tabs, obj);
 }
-
+function windowId(win)
+{
+	console.log(win.id);
+	return win.id;
+}
 function close(){
 	var selectedId = [];
-	// $("#activeTabs").children("checked").each(function(){ // separate tabs which are selected if button clicked 
-	// 	selectedId.push(obj[$(this).attr("name") + $(this).attr("num")]);	
-	// });
+
 	$(".tabs").each(function(){
 		if($(this).prop("checked")){
-			selectedId.push(obj[$(this).attr("name") + $(this).attr("num")]);
+			selectedId.push(parseInt($(this).attr("id"))); 
 		}
 	});
 	chrome.tabs.remove(selectedId);
 	//updateUI(tabs, obj);
 }
 
-function updateUI(tabs, obj){
+function updateUI(tabs, id){
 	//$(".tabs").emtpy();
+	var winId = $.cookie("winId");
+	winId += id.toString() + " ";
+	$.cookie("winId", winId);
 	for (var i = 0; i<tabs.length; i++)
 		{
-			obj[tabs[i].title + i.toString()] = tabs[i].id; 
 			var item = returnFormElement(tabs[i], i);
-			var s = generateLabelForFormElement(tabs[i]); // create label item for text
 			item.appendTo("#activeTabs"); // append form element
-			//item.after(s);	// append label
-			
 		}
 }
